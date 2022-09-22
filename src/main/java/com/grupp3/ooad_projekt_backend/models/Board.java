6e4e6d3c2f7a756e29ccf6e24d15409344e73a8b
Jan Beszterda <jan.beszterda@gmail.com>
2022-09-22 22:15:49 +0200

    Fixing messed up repo

---
+++ src/main/java/com/grupp3/ooad_projekt_backend/models/Board.java
@@ -0,0 +1,36 @@
+package com.grupp3.ooad_projekt_backend.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(	name = "boards",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "boardname")
+        })
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String boardName;
+
+    public Board(String boardName) {
+        this.boardName = boardName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBoardName() {
+        return boardName;
+    }
+
+    public void setBoardName(String boardName) {
+        this.boardName = boardName;
+    }
+}
