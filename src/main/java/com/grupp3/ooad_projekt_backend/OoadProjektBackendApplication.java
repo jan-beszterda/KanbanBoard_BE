d0e545a5d9e3c2761a8730f71a4ffa81c64a2b58
Jan Beszterda <jan.beszterda@gmail.com>
2022-09-22 21:40:39 +0200

    Initial commit

---
+++ src/main/java/com/grupp3/ooad_projekt_backend/OoadProjektBackendApplication.java
@@ -0,0 +1,13 @@
+package com.grupp3.ooad_projekt_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OoadProjektBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OoadProjektBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupp3/ooad_projekt_backend/OoadProjektBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupp3.ooad_projekt_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OoadProjektBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
