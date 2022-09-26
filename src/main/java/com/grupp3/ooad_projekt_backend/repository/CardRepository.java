f4035c4cc0a9177e3d6fc9ee9d7eafa12ea232f2
Damir1975 <91055438+Damir1975@users.noreply.github.com>
2022-09-26 10:52:20 +0200

    Card repository

--- src/main/java/com/grupp3/ooad_projekt_backend/repository/CardRepository.java
+++ src/main/java/com/grupp3/ooad_projekt_backend/repository/CardRepository.java
@@ -1,9 +1,6 @@
 package com.grupp3.ooad_projekt_backend.repository;
 
 import com.grupp3.ooad_projekt_backend.models.Card;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CardRepository extends JpaRepository<Card, Long> {
+import org.springframework.data.repository.CrudRepository;
+public interface CardRepository extends CrudRepository<Card, Long> {
 }
