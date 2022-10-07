266363e951f7d1c130a1c437df3ddd51c2c7b179
Jan Beszterda <jan.beszterda@gmail.com>
2022-10-07 23:43:04 +0200

    added socket controller for board, updated socket config, fixed add card method

--- src/main/java/com/grupp3/ooad_projekt_backend/service/CardService.java
+++ src/main/java/com/grupp3/ooad_projekt_backend/service/CardService.java
@@ -1,8 +1,10 @@
 package com.grupp3.ooad_projekt_backend.service;
 
+import com.grupp3.ooad_projekt_backend.dao.BoardDAO;
 import com.grupp3.ooad_projekt_backend.dao.CardDAO;
 import com.grupp3.ooad_projekt_backend.dao.ColumnDAO;
 import com.grupp3.ooad_projekt_backend.dao.UserDAO;
+import com.grupp3.ooad_projekt_backend.models.Board;
 import com.grupp3.ooad_projekt_backend.models.Card;
 import com.grupp3.ooad_projekt_backend.models.Column;
 import com.grupp3.ooad_projekt_backend.models.User;
@@ -18,11 +20,13 @@ public class CardService {
     private CardDAO cardDAO;
     private UserDAO userDAO;
     private ColumnDAO columnDAO;
+    private BoardDAO boardDAO;
 
-    public CardService(CardDAO cardDAO, UserDAO userDAO, ColumnDAO columnDAO) {
+    public CardService(CardDAO cardDAO, UserDAO userDAO, ColumnDAO columnDAO, BoardDAO boardDAO) {
         this.cardDAO = cardDAO;
         this.userDAO = userDAO;
         this.columnDAO = columnDAO;
+        this.boardDAO = boardDAO;
     }
 
     public List<Card> getAllCards() {
@@ -33,13 +37,20 @@ public class CardService {
         return cardDAO.findCardById(id).orElse(null);
     }
 
-    public Card addCard(Long userId, Long columnId, Card card) {
+    public Card addCard(Long userId, Long boardId, Long columnId, Card card) {
         Optional<User> maybeUser = userDAO.getUserById(userId);
         if (maybeUser.isEmpty()) {
             return null;
         }
         User user = maybeUser.get();
         card.setAuthor(user);
+
+        Optional<Board> maybeBoard = boardDAO.findBoardById(boardId);
+        if (maybeBoard.isEmpty()) {
+            return null;
+        }
+        Board board = maybeBoard.get();
+
         Optional<Column> maybeColumn = columnDAO.findColumnById(columnId);
         if(maybeColumn.isEmpty()) {
             return null;
@@ -48,9 +59,14 @@ public class CardService {
         if (null == column.getCardList()) {
             column.setCardList(new ArrayList<>());
         }
+
+        int index = board.getColumnList().indexOf(column);
+
         column.getCardList().add(card);
-        //columnDAO.saveColumn(column);
-        return cardDAO.saveCard(card);
+        board.getColumnList().remove(index);
+        board.getColumnList().add(index, column);
+        boardDAO.saveBoard(board);
+        return card;
     }
 
     public void removeCardById(Long id) {
